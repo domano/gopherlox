78eae1c0b4637ccd1e2887c2adc848c507b15f5c
Dino Omanovic <dino@theydo.com>
2024-10-08 20:33:47 +0200

    First lexer steps

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log/slog"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) > 2 {
+		slog.Info("Usage: gopherlox [script]")
+		os.Exit(64)
+	}
+	if len(os.Args) == 2 {
+		runFile(os.Args[1])
+	}
+	runPrompt()
+}
+
+func runFile(path string) {
+	var bytes, err = os.ReadFile(path)
+	if err != nil {
+		slog.Error("Error while running rile: %w", err)
+		return
+	}
+	run(string(bytes))
+}
+
+func runPrompt() {
+	var reader = bufio.NewReader(os.Stdin)
+	for {
+		print("> ")
+		line, err := reader.ReadString('\n')
+		if err != nil && err != io.EOF {
+			slog.Error("Error while reading input: %w", err)
+			return
+		}
+
+		if err == io.EOF {
+			break
+		}
+		run(line)
+
+	}
+}
+
+func run(source string) {
+	scanner := bufio.NewScanner(strings.NewReader(source))
+
+	// TODO we can add the lexing step here via a split function in the scanner
+	})
+	for {
+		if !scanner.Scan() {
+			break
+		}
+		slog.Info(scanner.Text())
+	}
+}
