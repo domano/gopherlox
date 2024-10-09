2f97c0eb31944c5d2141c4efd8322fed85a94706
Dino Omanovic <dino@theydo.com>
2024-10-09 21:01:09 +0200

    Introduce multiple token lexing into token slice, sans literals

--- main.go
+++ main.go
@@ -291,11 +291,15 @@ func run(source io.Reader) {
 				errMsg(line, "Unexpected end of file")
 				return
 			}
-			err := scanToken(lineString[start:current+1], line)
+			token, n, err := scanToken(lineString, start, current+1, line)
 			if errors.Is(err, ErrUnkownToken) {
 				continue
 			}
-			start++
+			Tokens = append(Tokens, token)
+			start += n
+			if start >= len(lineString) {
+				break
+			}
 		}
 		line++
 		for _, token := range Tokens {
@@ -307,54 +311,95 @@ func run(source io.Reader) {
 }
 
 func errMsg(line int, msg string) {
-	slog.Error("Error at line %d: %s", line, msg)
+	slog.Error(msg, "line", line)
 	hadError = true
 }
 
 var ErrUnkownToken = errors.New("unknown token")
 
-func scanToken(data []byte, line int) error {
-	switch string(data) {
+func scanToken(data []byte, start, end, line int) (Token, int, error) {
+	switch string(data[start:end]) {
 	case "(":
-		Tokens = append(Tokens, Token{Type: LEFT_PAREN, Lexeme: "(", Literal: "(", Line: line})
+		return Token{Type: LEFT_PAREN, Lexeme: "(", Literal: "(", Line: line}, end - start, nil
 	case ")":
-		Tokens = append(Tokens, Token{Type: RIGHT_PAREN, Lexeme: ")", Literal: ")", Line: line})
+		return Token{Type: RIGHT_PAREN, Lexeme: ")", Literal: ")", Line: line}, end - start, nil
 	case "{":
-		Tokens = append(Tokens, Token{Type: LEFT_BRACE, Lexeme: "{", Literal: "{", Line: line})
+		return Token{Type: LEFT_BRACE, Lexeme: "{", Literal: "{", Line: line}, end - start, nil
 	case "}":
-		Tokens = append(Tokens, Token{Type: RIGHT_BRACE, Lexeme: "}", Literal: "}", Line: line})
+		return Token{Type: RIGHT_BRACE, Lexeme: "}", Literal: "}", Line: line}, end - start, nil
 	case ",":
-		Tokens = append(Tokens, Token{Type: COMMA, Lexeme: ",", Literal: ",", Line: line})
+		return Token{Type: COMMA, Lexeme: ",", Literal: ",", Line: line}, end - start, nil
 	case ".":
-		Tokens = append(Tokens, Token{Type: DOT, Lexeme: ".", Literal: ".", Line: line})
+		return Token{Type: DOT, Lexeme: ".", Literal: ".", Line: line}, end - start, nil
 	case "-":
-		Tokens = append(Tokens, Token{Type: MINUS, Lexeme: "-", Literal: "-", Line: line})
+		return Token{Type: MINUS, Lexeme: "-", Literal: "-", Line: line}, end - start, nil
 	case "+":
-		Tokens = append(Tokens, Token{Type: PLUS, Lexeme: "+", Literal: "+", Line: line})
+		return Token{Type: PLUS, Lexeme: "+", Literal: "+", Line: line}, end - start, nil
 	case ";":
-		Tokens = append(Tokens, Token{Type: SEMICOLON, Lexeme: ";", Literal: ";", Line: line})
+		return Token{Type: SEMICOLON, Lexeme: ";", Literal: ";", Line: line}, end - start, nil
 	case "/":
-		Tokens = append(Tokens, Token{Type: SLASH, Lexeme: "/", Literal: "/", Line: line})
+		return Token{Type: SLASH, Lexeme: "/", Literal: "/", Line: line}, end - start, nil
 	case "*":
-		Tokens = append(Tokens, Token{Type: STAR, Lexeme: "*", Literal: "*", Line: line})
+		return Token{Type: STAR, Lexeme: "*", Literal: "*", Line: line}, end - start, nil
 	case "!":
-		Tokens = append(Tokens, Token{Type: BANG, Lexeme: "!", Literal: "!", Line: line})
-	case "!=":
-		Tokens = append(Tokens, Token{Type: BANG_EQUAL, Lexeme: "!=", Literal: "!=", Line: line})
+		return Token{Type: BANG, Lexeme: "!", Literal: "!", Line: line}, end - start, nil
 	case "=":
-		Tokens = append(Tokens, Token{Type: EQUAL, Lexeme: "=", Literal: "=", Line: line})
+		token, n, err := scanToken(data, start, end+1, line)
+		if !errors.Is(err, ErrUnkownToken) {
+			return token, n, err
+		}
+		return Token{Type: EQUAL, Lexeme: "=", Literal: "=", Line: line}, end - start, nil
+
+	case "!=":
+		return Token{Type: BANG_EQUAL, Lexeme: "!=", Literal: "!=", Line: line}, end - start, nil
 	case "==":
-		Tokens = append(Tokens, Token{Type: EQUAL_EQUAL, Lexeme: "==", Literal: "==", Line: line})
+		return Token{Type: EQUAL_EQUAL, Lexeme: "==", Literal: "==", Line: line}, end - start, nil
 	case ">":
-		Tokens = append(Tokens, Token{Type: GREATER, Lexeme: ">", Literal: ">", Line: line})
+		token, n, err := scanToken(data, start, end+1, line)
+		if !errors.Is(err, ErrUnkownToken) {
+			return token, n, err
+		}
+		return Token{Type: GREATER, Lexeme: ">", Literal: ">", Line: line}, end - start, nil
 	case ">=":
-		Tokens = append(Tokens, Token{Type: GREATER_EQUAL, Lexeme: ">=", Literal: ">=", Line: line})
+		return Token{Type: GREATER_EQUAL, Lexeme: ">=", Literal: ">=", Line: line}, end - start, nil
 	case "<":
-		Tokens = append(Tokens, Token{Type: LESS, Lexeme: "<", Literal: "<", Line: line})
+		token, n, err := scanToken(data, start, end+1, line)
+		if !errors.Is(err, ErrUnkownToken) {
+			return token, n, err
+		}
+		return Token{Type: LESS, Lexeme: "<", Literal: "<", Line: line}, end - start, nil
 	case "<=":
-		Tokens = append(Tokens, Token{Type: LESS_EQUAL, Lexeme: "<=", Literal: "<=", Line: line})
+		return Token{Type: LESS_EQUAL, Lexeme: "<=", Literal: "<=", Line: line}, end - start, nil
+	case "var":
+		return Token{Type: VAR, Lexeme: "var", Literal: "var", Line: line}, end - start, nil
+	case "class":
+		return Token{Type: CLASS, Lexeme: "class", Literal: "class", Line: line}, end - start, nil
+	case "super":
+		return Token{Type: SUPER, Lexeme: "super", Literal: "super", Line: line}, end - start, nil
+	case "this":
+		return Token{Type: THIS, Lexeme: "this", Literal: "this", Line: line}, end - start, nil
+	case "true":
+		return Token{Type: TRUE, Lexeme: "true", Literal: "true", Line: line}, end - start, nil
+	case "false":
+		return Token{Type: FALSE, Lexeme: "false", Literal: "false", Line: line}, end - start, nil
+	case "nil":
+		return Token{Type: NIL, Lexeme: "nil", Literal: "nil", Line: line}, end - start, nil
+	case "if":
+		return Token{Type: IF, Lexeme: "if", Literal: "if", Line: line}, end - start, nil
+	case "else":
+		return Token{Type: ELSE, Lexeme: "else", Literal: "else", Line: line}, end - start, nil
+	case "while":
+		return Token{Type: WHILE, Lexeme: "while", Literal: "while", Line: line}, end - start, nil
+	case "for":
+		return Token{Type: FOR, Lexeme: "for", Literal: "for", Line: line}, end - start, nil
+	case "fun":
+		return Token{Type: FUN, Lexeme: "fun", Literal: "fun", Line: line}, end - start, nil
+	case "return":
+		return Token{Type: RETURN, Lexeme: "return", Literal: "return", Line: line}, end - start, nil
+	case "print":
+		return Token{Type: PRINT, Lexeme: "print", Literal: "print", Line: line}, end - start, nil
 	default:
-		return ErrUnkownToken
+		errMsg(line, fmt.Sprintf("Unexpected char '%s'", string(data)))
+		return Token{}, 0, ErrUnkownToken
 	}
-	return nil
 }
